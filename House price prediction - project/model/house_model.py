bbd111c040ca7da485a8ab398351eb1c9105c0dc
EdwinVivek <nedwinvivek@gmail.com>
2025-01-07 20:41:32 +0530

    improvements

--- House price prediction - project/model/house_model.py
+++ House price prediction - project/model/house_model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 import pickle
 
 class HouseModel:
-    def __init__(self, features, target):
+    def __init__(self, features=None, target=None):
         self.features = features
         self.target = target
         self.x_train = None
